0c2c58b5cf049ddd28d99268e599b43f993f6120
MartialFlamme <ketchadjiguymartial@gmail.com>
2025-07-17 09:25:22 +0100

    Ajout du domaine Render à ALLOWED_HOSTS

--- bangoulap_site/settings.py
+++ bangoulap_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-u6&p#o!x@z@3xk=3)j#s6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["bangoulap-site.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['bangoulap-site.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
