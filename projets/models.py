56beeafc1fbf0f7d6682e4a582d56c5d32dee433
MartialFlamme <ketchadjiguymartial@gmail.com>
2025-08-02 17:53:52 +0100

    Mise à jour info

--- projets/models.py
+++ projets/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Projet(models.Model):
     titre = models.CharField(max_length=200)
     description = models.TextField()
-    date_lancement = models.DateField()
+    date_lancement = models.DateField(verbose_name="Date de lancement", blank=True, null=True)
     image = models.ImageField(upload_to='projets/images/', blank=True, null=True)
 
     def __str__(self):
