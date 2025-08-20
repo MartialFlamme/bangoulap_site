2665a973ef45c9901f88efa1f2c83d0c892f3431
MartialFlamme <ketchadjiguymartial@gmail.com>
2025-08-20 12:43:16 +0100

    mise a jour: lien

--- personnalites/views.py
+++ personnalites/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
 from .models import Personnalite
+from django.db.models import Q
 from .forms import PersonnaliteForm
 
 # 🌍 Liste publique
 def liste_personnalites(request):
+    q = request.GET.get('q', '')  # Récupère la valeur de la barre de recherche
     personnalites = Personnalite.objects.all()
+    
+    if q:
+        # Filtre par nom ou fonction contenant la chaîne recherchée (insensible à la casse)
+        personnalites = personnalites.filter(
+            Q(nom__icontains=q) | Q(fonction__icontains=q)
+        )
+    
     return render(request, 'personnalites/liste_personnalites.html', {'personnalites': personnalites})
 
 # 🌍 Détail public
